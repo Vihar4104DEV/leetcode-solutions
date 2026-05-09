e369bd86c97805b74bcada01f8998c1eb39e82b3
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:37:27 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,18 +1,18 @@
-# Submission timestamp: 2026-05-09T05:44:22.339Z
+# Submission timestamp: 2026-05-09T05:50:13.329Z
 
-4811121391015161457617181920        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.keys():            ans.append(i)
-d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.keys():            ans.append(i)
-d = {}
+811121391015161476175181920421        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        ma        return ans        d = {}
+for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        ma        return ans        d = {}
 for i in nums:
 if i in d:
 d[i] += 1
-if d[i] == 2:
-if len(ans) >= 2:
+if d[i] == k:
+if len(ans) >= k:
 return ans
 else:
 d[i] = 1
 ans.append(i)
-if k <=1:
 return [nums[0]]
-for i in d.keys():
-ans.append(i)
+if k <=1 and len(nums) == 1:
+ma
+return ans
+d = {}
