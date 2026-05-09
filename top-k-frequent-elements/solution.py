b15bece626ac14136eaaa0f832dd83e5fd8233a6
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:20:29 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,7 +1,7 @@
-# Submission timestamp: 2026-05-09T05:40:03.325Z
+# Submission timestamp: 2026-05-09T05:41:32.326Z
 
-1234581516179101114671213class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                d[i] == 2:                ans.append(i)            if len(ans) >= 2:                return ans            else:                d[]
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                d[i] == 2:                ans.append(i)            if len(ans) >= 2:                return ans            else:                d[]
+1234811121317910151614576class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return
+class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return
 class Solution:
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 ans = []
@@ -9,9 +9,11 @@ d = {}
 for i in nums:
 if i in d:
 d[i] += 1
-d[i] == 2:
-ans.append(i)
+if d[i] == 2:
 if len(ans) >= 2:
 return ans
 else:
-d[]
+d[i] = 1
+ans.append(i)
+if k <=1:
+return
