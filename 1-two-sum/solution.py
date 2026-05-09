6c06dbad78b2cdaa48ca3bf7dcaa4b158c8ebadd
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 18:44:26 +0530

    Updated: Two Sum in Python3

--- 1-two-sum/solution.py
+++ 1-two-sum/solution.py
@@ -1,17 +1,17 @@
-# Submission timestamp: 2026-05-09T13:13:59.362Z
-# Test cases: 102/102
+# Submission timestamp: 2026-05-09T13:13:59.380Z
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-
-        for ch in s:
-            if ch in "({[":
-                stack.append(ch)
-            else:
-                if not stack:
-                    return False
-                top = stack.pop()
-                if (ch == ')' and top != "(") or (ch == '}' and top != "{") or (ch == ']' and top != "["):
-                    return False
-        return not stack
+1234567891011121314151617class Solution:    def isValid(self, s: str) -> bool:        stack = []        for ch in s:            if ch in "({[":                stack.append(ch)            else:                if not stack:                    return False                top = stack.pop()                if (ch == ')' and top != "(") or (ch == '}' and top != "{") or (ch == ']' and top != "["):                    return False        return not stack
+class Solution:    def isValid(self, s: str) -> bool:        stack = []        for ch in s:            if ch in "({[":                stack.append(ch)            else:                if not stack:                    return False                top = stack.pop()                if (ch == ')' and top != "(") or (ch == '}' and top != "{") or (ch == ']' and top != "["):                    return False        return not stack
+class Solution:
+def isValid(self, s: str) -> bool:
+stack = []
+for ch in s:
+if ch in "({[":
+stack.append(ch)
+else:
+if not stack:
+return False
+top = stack.pop()
+if (ch == ')' and top != "(") or (ch == '}' and top != "{") or (ch == ']' and top != "["):
+return False
+return not stack
