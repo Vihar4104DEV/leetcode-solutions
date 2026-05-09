945f4b95c7d688e6b400ad6b188d52cf4e2bada2
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:35:30 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,9 +1,7 @@
-# Submission timestamp: 2026-05-09T05:40:40.325Z
+# Submission timestamp: 2026-05-09T05:42:20.326Z
 
-1234581591014671213111617class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)
-class Solution:
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+34811121391015161457617182        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 ans = []
 d = {}
 for i in nums:
@@ -15,3 +13,6 @@ return ans
 else:
 d[i] = 1
 ans.append(i)
+if k <=1:
+return [nums[0]]
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
