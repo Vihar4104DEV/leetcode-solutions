535a27f3652e54eb1fa6fcf2122b7266b8f075f8
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:29:34 +0530

    Solved: 0 in Python3

---
+++ -cyclically-rotating-a-grid/solution.py
@@ -0,0 +1,6 @@
+# Submission timestamp: 2026-05-09T05:59:32.979Z
+
+12class Solution:    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+class Solution:    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+class Solution:
+def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
