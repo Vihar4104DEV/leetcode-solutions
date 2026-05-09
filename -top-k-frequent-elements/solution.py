5fa721e95cd5643c93d48e1708ed9fac900f1077
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:20:38 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,19 +1,18 @@
-# Submission timestamp: 2026-05-09T05:42:10.336Z
+# Submission timestamp: 2026-05-09T05:50:37.334Z
 
-123481112131791015161457618class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]
-class Solution:
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-ans = []
-d = {}
+811121391015161476175181920421        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())            return ans        d = {}
+for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())            return ans        d = {}
 for i in nums:
 if i in d:
 d[i] += 1
-if d[i] == 2:
-if len(ans) >= 2:
+if d[i] == k:
+if len(ans) >= k:
 return ans
 else:
 d[i] = 1
 ans.append(i)
-if k <=1:
 return [nums[0]]
+if k <=1 and len(nums) == 1:
+max_values = max(d.values())
+return ans
+d = {}
