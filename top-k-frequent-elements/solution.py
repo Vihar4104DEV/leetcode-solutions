b1c1e5f548620a7ea259a5c51b4b9d6d9b9d0683
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:38:27 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,18 +1,16 @@
-# Submission timestamp: 2026-05-09T05:50:13.329Z
+# Submission timestamp: 2026-05-09T05:51:47.329Z
 
-811121391015161476175181920421        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        ma        return ans        d = {}
-for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        ma        return ans        d = {}
-for i in nums:
-if i in d:
-d[i] += 1
-if d[i] == k:
-if len(ans) >= k:
-return ans
+151614171819202122232413121110987            else:                d[i] = 1                    ans.append(i)            max_value = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return max_keys                if d[i] == k:                d[i] += 1            if i in d:                return ans            if len(ans) >= k:        for i in nums:
+else:                d[i] = 1                    ans.append(i)            max_value = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return max_keys                if d[i] == k:                d[i] += 1            if i in d:                return ans            if len(ans) >= k:        for i in nums:
 else:
 d[i] = 1
 ans.append(i)
-return [nums[0]]
-if k <=1 and len(nums) == 1:
-ma
+max_value = max(d.values())
+max_keys = [k for k, v in d.items() if v == max_value]
+return max_keys
+if d[i] == k:
+d[i] += 1
+if i in d:
 return ans
-d = {}
+if len(ans) >= k:
+for i in nums:
