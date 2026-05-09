dc128fe8f4fa2e62306b2eec8dd62f47720cde3c
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:34:54 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,6 +1,19 @@
-# Submission timestamp: 2026-05-09T05:37:29.327Z
+# Submission timestamp: 2026-05-09T05:50:42.331Z
 
-12345class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-class Solution:
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+81112139101516147617518192120        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return ans        return max
+for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return ans        return max
+for i in nums:
+if i in d:
+d[i] += 1
+if d[i] == k:
+if len(ans) >= k:
+return ans
+else:
+d[i] = 1
+ans.append(i)
+return [nums[0]]
+if k <=1 and len(nums) == 1:
+max_values = max(d.values())
+max_keys = [k for k, v in d.items() if v == max_value]
+return ans
+return max
