c9304556bc22745a227526235a16ca6852427ee7
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 18:23:06 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,7 +1,19 @@
-# Submission timestamp: 2026-05-09T05:37:37.326Z
+# Submission timestamp: 2026-05-09T05:45:10.328Z
 
-12345class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(len)
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(len)
-class Solution:
+8111213910151614761754321        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]                if k <=1 and len(nums) --:        d = {}        ans = []    def topKFrequent(self, nums: List[int], k: int) -> List[int]:class Solution:
+for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]                if k <=1 and len(nums) --:        d = {}        ans = []    def topKFrequent(self, nums: List[int], k: int) -> List[int]:class Solution:
+for i in nums:
+if i in d:
+d[i] += 1
+if d[i] == 2:
+if len(ans) >= 2:
+return ans
+else:
+d[i] = 1
+ans.append(i)
+return [nums[0]]
+if k <=1 and len(nums) --:
+d = {}
+ans = []
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-for i in range(len)
+class Solution:
