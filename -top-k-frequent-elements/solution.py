be57c72a884446a9fc19f6192eb861a05e6662d5
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 12:48:55 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,19 +1,7 @@
-# Submission timestamp: 2026-05-09T05:50:38.333Z
+# Submission timestamp: 2026-05-09T05:37:36.326Z
 
-811121391015161476175181920421        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return ans        d = {}
-for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return ans        d = {}
-for i in nums:
-if i in d:
-d[i] += 1
-if d[i] == k:
-if len(ans) >= k:
-return ans
-else:
-d[i] = 1
-ans.append(i)
-return [nums[0]]
-if k <=1 and len(nums) == 1:
-max_values = max(d.values())
-max_keys = [k for k, v in d.items() if v == max_value]
-return ans
-d = {}
+12345class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(lent())
+class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(lent())
+class Solution:
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+for i in range(lent())
