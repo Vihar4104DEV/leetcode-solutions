53c0b4dd24280f08a544141296645b8b5afb23b3
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:30:59 +0530

    Solved:  in Java

---
+++ -rotate-string/solution.java
@@ -0,0 +1,6 @@
+// Submission timestamp: 2026-05-09T06:00:57.978Z
+
+123class Solution:    def rotateString(self, s: str, goal: str) -> bool:
+class Solution:    def rotateString(self, s: str, goal: str) -> bool:
+class Solution:
+def rotateString(self, s: str, goal: str) -> bool:
