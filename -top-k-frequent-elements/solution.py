fe3aee6241f391b8f0ffe7fd212d8ced9ee9907c
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 18:22:05 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,7 +1,7 @@
-# Submission timestamp: 2026-05-09T05:37:36.326Z
+# Submission timestamp: 2026-05-09T05:37:37.326Z
 
-12345class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(lent())
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(lent())
+12345class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(len)
+class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        for i in range(len)
 class Solution:
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-for i in range(lent())
+for i in range(len)
