d0610f57ca5490892efd97d8842c0365708f9b72
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:21:46 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,18 +1,14 @@
-# Submission timestamp: 2026-05-09T05:50:37.334Z
+# Submission timestamp: 2026-05-09T05:51:45.333Z
 
-811121391015161476175181920421        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())            return ans        d = {}
-for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values())            return ans        d = {}
-for i in nums:
-if i in d:
-d[i] += 1
-if d[i] == k:
-if len(ans) >= k:
-return ans
+1516141718192021222324252613121110            else:                d[i] = 1                    ans.append(i)            max_value = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return max_keys                        if d[i] == k:                d[i] += 1            if i in d:                return ans
+else:                d[i] = 1                    ans.append(i)            max_value = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return max_keys                        if d[i] == k:                d[i] += 1            if i in d:                return ans
 else:
 d[i] = 1
 ans.append(i)
-return [nums[0]]
-if k <=1 and len(nums) == 1:
-max_values = max(d.values())
+max_value = max(d.values())
+max_keys = [k for k, v in d.items() if v == max_value]
+return max_keys
+if d[i] == k:
+d[i] += 1
+if i in d:
 return ans
-d = {}
