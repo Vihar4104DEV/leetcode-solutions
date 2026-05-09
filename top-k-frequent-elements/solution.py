07ce3b530cfccf92fd716d71769f39aa3933e29c
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:22:32 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,9 +1,7 @@
-# Submission timestamp: 2026-05-09T05:44:17.326Z
+# Submission timestamp: 2026-05-09T05:45:09.346Z
 
-348111213910151614576171819        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.keys()
-ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.keys()
-ans = []
-d = {}
+8111213910151614761754321        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]                if k <=1 and len(nums) :        d = {}        ans = []    def topKFrequent(self, nums: List[int], k: int) -> List[int]:class Solution:
+for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]                if k <=1 and len(nums) :        d = {}        ans = []    def topKFrequent(self, nums: List[int], k: int) -> List[int]:class Solution:
 for i in nums:
 if i in d:
 d[i] += 1
@@ -13,6 +11,9 @@ return ans
 else:
 d[i] = 1
 ans.append(i)
-if k <=1:
 return [nums[0]]
-for i in d.keys()
+if k <=1 and len(nums) :
+d = {}
+ans = []
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+class Solution:
