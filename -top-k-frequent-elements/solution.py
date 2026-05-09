757933fdfbda53d25cf9b81cad05dbeffe9261f3
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:14:06 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,7 +1,7 @@
-# Submission timestamp: 2026-05-09T05:42:25.366Z
+# Submission timestamp: 2026-05-09T05:44:06.328Z
 
-348111213910151614576171819        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                f
-ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                f
+348111213910151614576171819        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.items
+ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.items
 ans = []
 d = {}
 for i in nums:
@@ -15,4 +15,4 @@ d[i] = 1
 ans.append(i)
 if k <=1:
 return [nums[0]]
-f
+for i in d.items
