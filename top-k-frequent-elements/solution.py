a8772f8efdfe860fdab8ec2f80924decb9a5e1b4
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:39:27 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,16 +1,9 @@
-# Submission timestamp: 2026-05-09T05:51:47.329Z
+# Submission timestamp: 2026-05-09T05:54:47.325Z
 
-151614171819202122232413121110987            else:                d[i] = 1                    ans.append(i)            max_value = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return max_keys                if d[i] == k:                d[i] += 1            if i in d:                return ans            if len(ans) >= k:        for i in nums:
-else:                d[i] = 1                    ans.append(i)            max_value = max(d.values())        max_keys = [k for k, v in d.items() if v == max_value]        return max_keys                if d[i] == k:                d[i] += 1            if i in d:                return ans            if len(ans) >= k:        for i in nums:
-else:
-d[i] = 1
-ans.append(i)
-max_value = max(d.values())
-max_keys = [k for k, v in d.items() if v == max_value]
-return max_keys
-if d[i] == k:
-d[i] += 1
-if i in d:
-return ans
-if len(ans) >= k:
-for i in nums:
+123456class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        freq = {}        for num in nums:            freq
+class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        freq = {}        for num in nums:            freq
+class Solution:
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+freq = {}
+for num in nums:
+freq
