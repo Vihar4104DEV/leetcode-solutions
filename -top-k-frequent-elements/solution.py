477abc558fdf74d2ae310e834c0a2ca9752fc43f
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 11:33:55 +0530

    Updated:  in Python3

--- -top-k-frequent-elements/solution.py
+++ -top-k-frequent-elements/solution.py
@@ -1,11 +1,6 @@
-# Submission timestamp: 2026-05-09T05:56:21.328Z
+# Submission timestamp: 2026-05-09T05:37:29.327Z
 
-1234567891011class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        freq = {}        for num in nums:            freq[num] = freq.get(num,0) + 1        s_nums = sorted(freq, key=freq.get, reverse=True)        return s_nums[:k]
-class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:        freq = {}        for num in nums:            freq[num] = freq.get(num,0) + 1        s_nums = sorted(freq, key=freq.get, reverse=True)        return s_nums[:k]
+12345class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+class Solution:    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 class Solution:
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-freq = {}
-for num in nums:
-freq[num] = freq.get(num,0) + 1
-s_nums = sorted(freq, key=freq.get, reverse=True)
-return s_nums[:k]
