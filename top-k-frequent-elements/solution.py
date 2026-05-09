3eb0be8a871101893847ea0c1d6e699d3222ba83
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:23:33 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,19 +1,18 @@
-# Submission timestamp: 2026-05-09T05:45:09.346Z
+# Submission timestamp: 2026-05-09T05:50:24.335Z
 
-8111213910151614761754321        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]                if k <=1 and len(nums) :        d = {}        ans = []    def topKFrequent(self, nums: List[int], k: int) -> List[int]:class Solution:
-for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]                if k <=1 and len(nums) :        d = {}        ans = []    def topKFrequent(self, nums: List[int], k: int) -> List[int]:class Solution:
+811121391015161476175181920421        for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values)        return ans        d = {}
+for i in nums:            if i in d:                d[i] += 1                if d[i] == k:            if len(ans) >= k:                return ans            else:                d[i] = 1                    ans.append(i)            return [nums[0]]            if k <=1 and len(nums) == 1:        max_values = max(d.values)        return ans        d = {}
 for i in nums:
 if i in d:
 d[i] += 1
-if d[i] == 2:
-if len(ans) >= 2:
+if d[i] == k:
+if len(ans) >= k:
 return ans
 else:
 d[i] = 1
 ans.append(i)
 return [nums[0]]
-if k <=1 and len(nums) :
+if k <=1 and len(nums) == 1:
+max_values = max(d.values)
+return ans
 d = {}
-ans = []
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-class Solution:
