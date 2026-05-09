f03d783b01f679c45c5aab5dd58241fdf7f48a89
Vihar Talaviya <vihar.talaviya.dev@gmail.com>
2026-05-09 19:36:27 +0530

    Updated:  in Python3

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,8 +1,7 @@
-# Submission timestamp: 2026-05-09T05:42:20.326Z
+# Submission timestamp: 2026-05-09T05:44:22.339Z
 
-34811121391015161457617182        ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-ans = []        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-ans = []
+4811121391015161457617181920        d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.keys():            ans.append(i)
+d = {}        for i in nums:            if i in d:                d[i] += 1                if d[i] == 2:            if len(ans) >= 2:                return ans            else:                d[i] = 1                    ans.append(i)        if k <=1:            return [nums[0]]                for i in d.keys():            ans.append(i)
 d = {}
 for i in nums:
 if i in d:
@@ -15,4 +14,5 @@ d[i] = 1
 ans.append(i)
 if k <=1:
 return [nums[0]]
-def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+for i in d.keys():
+ans.append(i)
